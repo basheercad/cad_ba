730015e7aa20a9cabb997eade5af377a2587f0ee
basheercad01 <basheercad01@gmail.com>
2024-01-05 18:02:06 +0530

    js modified

--- new.js
+++ new.js
@@ -1,5 +1,73 @@
-<script>
-    var a-=10;
-    var b= 10;
-    console.log(a+b);
-</script>
+
+    a = 7; 
+    a += 5; // a = a + 5
+    a -= 2; // a = a - 2
+    a *= 3; // a = a * 3
+    a /= 2; // a = a / 2
+    a %= 2; // a = a % 2
+    a **= 2; // a = a**2
+
+b=5;
+x=5;
+y=3;
+// addition
+console.log('x + y = ', x + y);  // 8
+
+// subtraction
+console.log('x - y = ', x - y);  // 2
+
+// multiplication
+console.log('x * y = ', x * y);  // 15
+
+// division
+console.log('x / y = ', x / y);  // 1.6666666666666667
+
+// remainder
+console.log('x % y = ', x % y);   // 2
+
+// increment
+console.log('++x = ', ++x); // x is now 6
+console.log('x++ = ', x++); // prints 6 and then increased to 7
+console.log('x = ', x);     // 7
+
+// decrement
+console.log('--x = ', --x); // x is now 6
+console.log('x-- = ', x--); // prints 6 and then decreased to 5
+console.log('x = ', x);     // 5
+
+//exponentiation
+console.log('x ** y =', x ** y);
+
+
+// equal operator
+console.log(2 == 2); // true
+console.log(2 == '2'); // true
+
+// not equal operator
+console.log(3 != 2); // true
+console.log('hello' != 'Hello'); // true
+
+// strict equal operator
+console.log(2 === 2); // true
+console.log(2 === '2'); // false
+
+// strict not equal operator
+console.log(2 !== '2'); // true
+console.log(2 !== 2); // false
+// logical AND
+console.log(true && true); // true
+console.log(true && false); // false
+
+// logical OR
+console.log(true || false); // true
+
+// logical NOT
+console.log(!true); // false
+// concatenation operator
+console.log('hello' + 'world');
+
+let d = 'JavaScript';
+
+d += ' tutorial';  // a = a + ' tutorial';
+console.log(a);
+
