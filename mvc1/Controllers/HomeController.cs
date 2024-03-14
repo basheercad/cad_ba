8842340f2ad87b9b90bfc4cf3289ba0cc3f7233a
basheercad01 <basheercad01@gmail.com>
2024-03-14 11:34:41 +0530

    add

---
+++ mvc1/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using mvc1.Models;
+
+namespace mvc1.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+     public IActionResult playlist()
+    {
+        return View();
+    }
+
+     public IActionResult Login()
+    {
+        return View();
+    }
+
+     public IActionResult profile()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
